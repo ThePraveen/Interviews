991fef25975d8eebcbed085c45ff6f2376f323ba
Praveen Sah <thepraveen0207@gmail.com>
2019-02-22 13:39:03 +0530

    Create ternary_exp_2_to_bt.java

---
+++ ternary_exp_2_to_bt.java
@@ -0,0 +1,18 @@
+
+/*
+Given a string that contains ternary expressions. The expressions may be nested, task is convert the given ternary expression to a binary Tree.
+
+Examples:
+
+Input :  string expression =   a?b:c 
+Output :        a
+              /  \
+             b    c
+
+Input : expression =  a?b?c:d:e
+Output :     a
+           /  \
+          b    e
+        /  \
+       c    d
+       */
